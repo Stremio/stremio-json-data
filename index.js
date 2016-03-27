e6b55707734927324ea2256b7a5a6d5796df49fd
Ivo Georgiev <ivo@strem.io>
2016-03-27 16:14:17 +0300

    add ability to get all json's from index

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports={}
+module.exports = {
+  languageNames: require("./languagesNames.json"),
+  languageMap: require("./languagesISO639-to-ISO639-2.json"),
+  languageForCountry: require("./languagesForCountryISO639-2.json")
+}
